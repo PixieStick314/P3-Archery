c060f401dbd2f3c68a8d4846400d8c34afb4a7ed
PixieStick314 <bn16nx@student.aau.dk>
2023-10-10 12:21:37 +0200

    modified:   src/user/GuestUser.java 	modified:   src/user/User.java

--- src/user/GuestUser.java
+++ src/user/GuestUser.java
@@ -1,5 +1,13 @@
 package src.user;
 
+import java.util.Date;
+
 public class GuestUser extends User{
+
+    public GuestUser(String name, String gender, String address, int postcode, int cellphoneNr, Date dateOfBirth,
+            String eMail, int archerySkillLevel, int accessLevel, boolean hasLocker) {
+        super(name, gender, address, postcode, cellphoneNr, dateOfBirth, eMail, archerySkillLevel, accessLevel, hasLocker);
+        //TODO Auto-generated constructor stub
+    }
     
 }
