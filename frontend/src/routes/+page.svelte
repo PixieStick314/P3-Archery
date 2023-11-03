15a6c326c07f049a6ff34e107f4a10ecae1df841
Loke S. G. Jørgensen <lsgj21@student.aau.dk>
2023-11-03 14:23:15 +0100

    moved event creation to a hidden endpoints accessible by button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1 class="container-sm">Hej medlem!</h1>
 
-<div class="container-sm">
+<div class="container-sm mt-3">
 <button type="button" class="btn btn-primary" onclick="window.location.href='/events'">Opret begivenhed</button>
 </div>
