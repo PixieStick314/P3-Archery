bf59a613fe858dbe971a1a6e7fbc70ae9a4b5ffe
Ronald Johnson <ronaldhej@gmail.com>
2023-12-17 05:19:23 +0100

    removed old commented out code from training and user

--- src/main/java/P3/Archery/model/Training.java
+++ src/main/java/P3/Archery/model/Training.java
@@ -34,14 +34,4 @@ public class Training extends Event{
         }
     }
 
-/*
-    public Training repeatTraining(Training training){
-        String newID = "0"; //ID assignment placeholder
-        ZonedDateTime newStartTime = training.startTime.plusDays(7);
-        ZonedDateTime newEndTime = training.endTime.plusDays(7);
-        Training newTraining = new Training(training, training.trainers);
-        return newTraining;
-    }
-*/
-
 }
